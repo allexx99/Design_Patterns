f463f02fdff0e6af1f4916272e1d532cb0baf410
Alex Usuc <alexusuc@yahoo.com>
2024-09-26 09:53:46 +0300

    Implemented Singleton Pattern

---
+++ Singleton/src/Singleton.java
@@ -0,0 +1,30 @@
+public class Singleton {
+    // this field must be declared volatile so that double check lock would work correctly
+    private static volatile Singleton instance;
+    public String value;
+
+    private Singleton(String value) {
+        this.value = value;
+    }
+
+    public static Singleton getInstance(String value) {
+        // the approach taken here is called double-checked locking (DCL)
+        // it exists to prevent race condition between multiple threads
+        // that may attempt to get singleton instance at the same time
+        // creating separate instances as a result.
+        //
+        // it may seem that having the 'result' variable here is completely pointless.
+        // there is a very important caveat when implementing double-checked locking in Java,
+        // which is solved by introducing this local variable
+        Singleton result = instance;
+        if(result != null) {
+            return result;
+        }
+        synchronized (Singleton.class) {
+            if(instance == null) {
+                instance = new Singleton(value);
+            }
+            return instance;
+        }
+    }
+}
